bd35bae8e8381bd87e27588bc9d87813e19c4aa4
Eric Fithian <ef2413@columbia.edu>
2022-06-22 19:07:04 -0400

    Artist create added two ways

--- tunr/urls.py
+++ tunr/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.artist_list, name='artist_list'),
+    path('artists/', views.artist_list, name='artist_list'),
     path('artists/<int:pk>/', views.artist_detail, name='artist_detail'),
+    # path('artists/new', views.artist_create, name='artist_create'),
+    path('artists/new', views.ArtistCreate.as_view(), name='artist_create'),
     path('songs/', views.song_list, name='song_list'),
     path('songs/<int:pk>/', views.song_detail, name='song_detail'),
 ]
--- tunr/views.py
+++ tunr/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Artist, Song
+# from .forms import ArtistForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+class ArtistCreate(CreateView):
+    model = Artist
+    fields = ['name', 'photo_url', 'nationality']
+    template_name = 'tunr/artist_form.html'
+    success_url = '/artists/'
 
 def artist_list(request):
     artists = Artist.objects.all()
@@ -18,3 +26,13 @@ def artist_detail(request, pk):
 def song_detail(request, pk):
     song = Song.objects.get(id=pk)
     return render(request, 'tunr/song_detail.html', {'song': song})
+
+# def artist_create(request):
+#     if request.method == 'POST':
+#         form = ArtistForm(request.POST)
+#         if form.is_valid():
+#             artist = form.save()
+#             return redirect('artist_detail', pk=artist.pk)
+#     else:
+#         form = ArtistForm()
+#     return render(request, 'tunr/artist_form.html', {'form': form})
